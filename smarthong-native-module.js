937473fc81c744c1b4e7f08fbfdd45a1221d099f
Andrew Lee <inf_sky@naver.com>
2019-01-24 15:08:48 +0900

    Create smarthong-native-module.js
    스마트통 js 모듈

---
+++ smarthong-native-module.js
@@ -0,0 +1,51 @@
+'user strict'
+
+const type_android = 'ANDROID';
+const type_ios = 'iOS'
+const type_web = 'WEB'
+
+var smNative = {
+    registerAccessToken : function(param) {
+        switch (getHandler()) {
+            case type_android: androidHandler.registerAccessToken(param);
+                console.log("androidHandler.registerAccessToken(param)");
+                break;
+            case type_ios: iOSHandler.postMessage({ command: "registerAccessToken", value: param });
+                console.log('iOSHandler.postMessage({ command : "registerAccessToken", value : param});');
+                break;
+        }
+    },
+    requestLogout: function () {
+        switch (getHandler()) {
+            case type_android: androidHandler.requestLogout();
+                console.log("androidHandler.requestLogout()");
+                break;
+            case type_ios: iOSHandler.postMessage({ command: "requestLogout" })
+                console.log(`iOSHandler.postMessage({command: "requestLogout"})`)
+                break;
+        }
+    }
+}
+
+function getHandler() {
+  if (typeof window === 'undefined') {
+    // SSR(Server Side Rendering)일 때 작동하지 않도록 방어
+    return console.log("SSR.");
+  }
+  
+if (window.smarthong) {
+    // Call Android interface
+    androidHandler = window.smarthong;
+    return type_android;
+  } else if (window.webkit
+      && window.webkit.messageHandlers
+      && window.webkit.messageHandlers.smarthong) {
+    // Call iOS interface
+    iOSHandler = window.webkit.messageHandlers.smarthong;
+    return type_ios;
+  } else {
+    // No Android or iOS interface found
+    console.log("No native APIs found.");
+    return type_web
+  }
+}
